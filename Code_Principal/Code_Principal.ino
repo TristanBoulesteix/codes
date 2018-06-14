4727b58cb92704c0c59b7ae85d1a8a7aaa96c39f
Ssaubeck <guillaume.best@viacesi.fr>
2018-06-14 23:30:48 +0200

    Code Principale
    Setup des 4 capteurs
    détections avec deux capteurs pour rester centré sur la route

--- Code_Principal/Code_Principal.ino
+++ Code_Principal/Code_Principal.ino
@@ -2,24 +2,35 @@
 
 #define I2C_ADDRESS 0x0f
 
-int signalPin = 8;
+int signalPinC1 = 8;
+int signalPinC2 = 4;
+int signalPinLg = 6;
+int signalPinLd = 2;
 
 void setup() {
   Motor.begin(I2C_ADDRESS);
   Serial.begin(9600);
-  pinMode(signalPin, INPUT);
+  pinMode(signalPinC1, INPUT);
+  pinMode(signalPinC2, INPUT);
+  pinMode(signalPinLg, INPUT);
+  pinMode(signalPinLd, INPUT);
+  
 }
 
 void loop() {
   delay(200);
-  if (HIGH == digitalRead(signalPin)) {
-    moveForward();
-    turnRight();
-    Serial.println("black");
-  } else {
-
-    Serial.println("white");
-    stopMotors();
+  if (digitalRead(signalPinC1)== HIGH && digitalRead(signalPinC2)== HIGH){
+    Serial.println("Tout Droit");
+    //moveForward();
+    //turnRight();
+  }
+  
+  if (digitalRead(signalPinC1)== HIGH && digitalRead(signalPinC2)== LOW){
+    Serial.println("A gauche");
+  }
+  
+  if (digitalRead(signalPinC1)== LOW && digitalRead(signalPinC2)== HIGH){
+    Serial.println("A droite");
   }
 }
 
