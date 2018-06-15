f868eb40fb8da528ec6e39253dd0820480fe6861
Ssaubeck <guillaume.best@viacesi.fr>
2018-06-15 16:52:35 +0200

    Code_Principale
    Ajout de la fonction state permettant de définir les etats du vehicule

--- Code_Principal/Code_Principal.ino
+++ Code_Principal/Code_Principal.ino
@@ -2,46 +2,27 @@
 
 #define I2C_ADDRESS 0x0f
 
+//Constants
+const int STRAIGHT = 100;
+const int TURN = 200;
+const int LEFT = 210;
+const int RIGHT = 220;
+const int PROBLEM = 300;
+const int CORRECT = 400;
+
+
+// Integer
 int signalPinC1 = 8;
 int signalPinC2 = 4;
-int signalPinLg = 7;
-int signalPinLd = 6;
-
-void setup() {
-  Motor.begin(I2C_ADDRESS);
-  Serial.begin(9600);
-  pinMode(signalPinC1, INPUT);
-  pinMode(signalPinC2, INPUT);
-  pinMode(signalPinLg, INPUT);
-  pinMode(signalPinLd, INPUT);
-  delay(200);
-}
-
-void loop() {
-  if (digitalRead(signalPinC1)== HIGH && digitalRead(signalPinC2)== HIGH){
-    Serial.println("Tout Droit");
-    moveForward();
-  }
-  
-  if (digitalRead(signalPinC1)== HIGH && digitalRead(signalPinC2)== LOW){
-    Serial.println("A gauche");
-    turnLeft();
-  }
-  
-  if (digitalRead(signalPinC1)== LOW && digitalRead(signalPinC2)== HIGH){
-    Serial.println("A droite");
-    turnRight();
-  }
-  if (digitalRead(signalPinC1)== LOW && digitalRead(signalPinC2)== LOW){
-    stopMotors();
-  }
-  
-}
+int signalPinLg = 6;
+int signalPinLd = 2;
+int statut = STRAIGHT;
+int statutT;
 
 void moveForward() {
-  Motor.speed(MOTOR1, 100);
-  Motor.speed(MOTOR2, 88);
-  
+  Motor.speed(MOTOR1, 50);
+  Motor.speed(MOTOR2, 44);
+
 }
 
 void stopMotors() {
@@ -49,13 +30,109 @@ void stopMotors() {
   Motor.stop(MOTOR2);
 }
 
-void turnRight() {
-  Motor.speed(MOTOR1, 100);
-  Motor.speed(MOTOR2, 28);
+void correctRight() {
+  Motor.speed(MOTOR1, 50);
+  Motor.speed(MOTOR2, 14);
+}
+
+void correctLeft() {
+  Motor.speed(MOTOR1, 20);
+  Motor.speed(MOTOR2, 44);
 }
 
 void turnLeft() {
-  Motor.speed(MOTOR1, 40);
-  Motor.speed(MOTOR2, 88);
+  Motor.speed(MOTOR1, -50);
+  Motor.speed(MOTOR2, 44);
+}
+
+void turnRight() {
+  Motor.speed(MOTOR1, 50);
+  Motor.speed(MOTOR2, -44);
+}
+
+void state(){
+  if (statut == STRAIGHT)
+  {
+    if (digitalRead(signalPinLg) == LOW && digitalRead(signalPinC1) == LOW && digitalRead(signalPinC2) == LOW && digitalRead(signalPinLd) == LOW)
+    {
+      //a long terme passé cette possibilité à recherche de ligne
+      moveForward();
+    }
+
+    else if (digitalRead(signalPinLg) == LOW && digitalRead(signalPinC1) == LOW && digitalRead(signalPinC2) == HIGH && digitalRead(signalPinLd) == LOW)
+    {
+      // etat de redressement sur la ligne DROITE
+      statut = CORRECT;
+      statutT = RIGHT;
+    }
+
+    else if (digitalRead(signalPinLg) == LOW && digitalRead(signalPinC1) == HIGH && digitalRead(signalPinC2) == LOW && digitalRead(signalPinLd) == LOW)
+    {
+      // etat de redressement sur la ligne GAUCHE
+      statut = CORRECT;
+      statutT = LEFT;
+    }
+    else if (digitalRead(signalPinLg) == LOW && digitalRead(signalPinC1) == HIGH && digitalRead(signalPinC2) == HIGH && digitalRead(signalPinLd) == HIGH)
+    {
+      // etat de tourner à droite
+      statut = TURN;
+      statutT = RIGHT;
+    }
+    else if (digitalRead(signalPinLg) == LOW && digitalRead(signalPinC1) == HIGH && digitalRead(signalPinC2) == HIGH && digitalRead(signalPinLd) == LOW)
+    {
+      // avancement normal
+      moveForward();
+    }
+    else if (digitalRead(signalPinLg) == LOW && digitalRead(signalPinC1) == HIGH && digitalRead(signalPinC2) == HIGH && digitalRead(signalPinLd) == HIGH)
+    {
+      //etat de direction aléatoire entre tout droit et à droite
+    }
+    else if (digitalRead(signalPinLg) == HIGH && digitalRead(signalPinC1) == LOW && digitalRead(signalPinC2) == LOW && digitalRead(signalPinLd) == LOW)
+    {
+      //etat de direction aléatoire entre à gauche et à droite
+      statut = TURN;
+      // passe la statutT à la valeur trouvé avec l'aléatoire
+    }
+    else if (digitalRead(signalPinLg) == HIGH && digitalRead(signalPinC1) == HIGH && digitalRead(signalPinC2) == HIGH && digitalRead(signalPinLd) == LOW)
+    {
+      //etat de tourner à gauche
+      statut = TURN;
+      statutT = LEFT;
+    }
+    else if (digitalRead(signalPinLg) == HIGH && digitalRead(signalPinC1) == HIGH && digitalRead(signalPinC2) == HIGH && digitalRead(signalPinLd) == HIGH)
+    {
+      //etat de tourner à gauche ou à droite ou bien continué d'aller tout droit
+      // set le statut à TURN ou STRAIGHT statut
+      //set le statutT à LEFT ou RIGHT uniquement si TURN est selectionné
+    }
+    else {
+      statut = PROBLEM;
+    }
+
+  }
+  else if (statut == TURN) {
+
+  }
+  else if ( statut == CORRECT) {
+
+  }
+}
+
+
+
+void setup() {
+  Motor.begin(I2C_ADDRESS);
+  pinMode(signalPinC1, INPUT);
+  pinMode(signalPinC2, INPUT);
+  pinMode(signalPinLg, INPUT);
+  pinMode(signalPinLd, INPUT);
+  Serial.begin(9600);
+  delay(200);
+}
+
+
+void loop() 
+{
+  state ();
 }
 
