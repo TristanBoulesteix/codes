f5f34c2c81762d7b912f8c7c9503bdb3f532416d
Ssaubeck <guillaume.best@viacesi.fr>
2018-06-17 15:33:59 +0200

    update code pour aller tout droit

--- Anciennes versions code principal/Code_Principal_V1/Code_Principal_V1.ino
+++ Anciennes versions code principal/Code_Principal_V1/Code_Principal_V1.ino
@@ -7,36 +7,6 @@ int signalPinC2 = 4;
 int signalPinLg = 7;
 int signalPinLd = 6;
 
-void setup() {
-  Motor.begin(I2C_ADDRESS);
-  Serial.begin(9600);
-  pinMode(signalPinC1, INPUT);
-  pinMode(signalPinC2, INPUT);
-  pinMode(signalPinLg, INPUT);
-  pinMode(signalPinLd, INPUT);
-  delay(200);
-}
-
-void loop() {
-  if (digitalRead(signalPinC1)== HIGH && digitalRead(signalPinC2)== HIGH){
-    Serial.println("Tout Droit");
-    moveForward();
-  }
-  
-  if (digitalRead(signalPinC1)== HIGH && digitalRead(signalPinC2)== LOW){
-    Serial.println("A gauche");
-    turnLeft();
-  }
-  
-  if (digitalRead(signalPinC1)== LOW && digitalRead(signalPinC2)== HIGH){
-    Serial.println("A droite");
-    turnRight();
-  }
-  if (digitalRead(signalPinC1)== LOW && digitalRead(signalPinC2)== LOW){
-    stopMotors();
-  }
-  
-}
 
 void moveForward() {
   Motor.speed(MOTOR1, 50);
@@ -49,13 +19,45 @@ void stopMotors() {
   Motor.stop(MOTOR2);
 }
 
-void turnRight() {
+void correctRight() {
   Motor.speed(MOTOR1, 50);
   Motor.speed(MOTOR2, 12);
 }
 
-void turnLeft() {
+void correctLeft() {
   Motor.speed(MOTOR1, 20);
   Motor.speed(MOTOR2, 44);
 }
+
+void setup() {
+  Motor.begin(I2C_ADDRESS);
+  Serial.begin(9600);
+  pinMode(signalPinC1, INPUT);
+  pinMode(signalPinC2, INPUT);
+  pinMode(signalPinLg, INPUT);
+  pinMode(signalPinLd, INPUT);
+  delay(200);
+}
+
+void loop() {
+  if (digitalRead(signalPinLg)==LOW && digitalRead(signalPinC1)== HIGH && digitalRead(signalPinC2)== HIGH && digitalRead(signalPinLg)==LOW){
+    Serial.println("Tout Droit");
+    moveForward();
+  }
+  
+  if (digitalRead(signalPinLg)==LOW && digitalRead(signalPinC1)== HIGH && digitalRead(signalPinC2)== LOW && digitalRead(signalPinLg)==LOW){
+    Serial.println("A gauche");
+    correctLeft();
+  }
+  
+  if (digitalRead(signalPinLg)==LOW && digitalRead(signalPinC1)== LOW && digitalRead(signalPinC2)== HIGH && digitalRead(signalPinLg)==LOW){
+    Serial.println("A droite");
+    correctRight();
+  }
+  if (digitalRead(signalPinLg)==LOW && signalPinC1)== LOW && digitalRead(signalPinC2)== LOW && digitalRead(signalPinLg)==LOW){
+    stopMotors();
+  }
+  
+}
+
 
