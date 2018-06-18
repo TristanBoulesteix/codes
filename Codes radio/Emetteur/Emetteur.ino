6f0b36456cbef2f524fe03646d5844951dd6180e
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-06-18 14:31:19 +0200

    Update

--- Codes radio/Emetteur/Emetteur.ino
+++ Codes radio/Emetteur/Emetteur.ino
@@ -1,79 +1,33 @@
-
-
 /**
-
-  Exemple de code pour la bibliothèque VirtualWire – Serveur d'envoi de texte
-
-*/
-
-
-
+ * Exemple de code pour la bibliothèque VirtualWire – Client d'envoi de texte
+ */
+ 
 #include <VirtualWire.h>
-
 
 int RF_TX_PIN = 2;
 
 void setup() {
-
   Serial.begin(9600);
   vw_set_tx_pin(RF_TX_PIN);
 
-
   // Initialisation de la bibliothèque VirtualWire
-
   // Vous pouvez changez les broches RX/TX/PTT avant vw_setup() si nécessaire
-
   vw_setup(2000);
-
-  vw_rx_start(); // On peut maintenant recevoir des messages
-
-
-
-  Serial.println("Go !");
-
+  
+  Serial.println("Go !"); 
 }
-
-
-
+ 
 void loop() {
-
-  byte message[VW_MAX_MESSAGE_LEN];
-
-  byte taille_message = VW_MAX_MESSAGE_LEN;
-
+  byte message[VW_MAX_MESSAGE_LEN]; 
   // N.B. La constante VW_MAX_MESSAGE_LEN est fournie par la lib VirtualWire
-
-
-
-  /*
-
-    La variable "taille_message" doit impérativement être remise à
-
-    la taille du buffer avant de pouvoir recevoir un message.
-
-    Le plus simple est d'utiliser une variable locale pour ne pas
-
-    avoir à réassigner la valeur à chaque début de loop().
-
-  */
-
-
-
-  // On attend de recevoir un message
-
-  vw_wait_rx();
-
-
-
-  if (vw_get_message(message, &taille_message)) {
-
-    // On copie le message, qu'il soit corrompu ou non
-
-
-
-    Serial.println((char*) message); // Affiche le message
-
+  
+  // Lit un message de maximum 26 caractères depuis le port série
+  int len = Serial.readBytesUntil('\n', (char*) message, VW_MAX_MESSAGE_LEN - 1);
+  if (!len) {
+    return; // Pas de message
   }
-
-}
-
+  message[len] = '\0'; // Ferme la chaine de caractères
+  
+  vw_send(message, len + 1); // On envoie le message
+  vw_wait_tx(); // On attend la fin de l'envoi
+} 
