ad65a613e8b6c355d83151a69d1bfdd687d289ee
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-06-19 10:45:11 +0200

    update

--- Codes radio/Emetteur_Tests/Emetteur_Tests.ino
+++ Codes radio/Emetteur_Tests/Emetteur_Tests.ino
@@ -8,22 +8,23 @@ int RF_TX_PIN = 2;
 
 typedef struct header {
   unsigned char id[4] = {6, 5, 0, 0};
-  unsigned int cheksum;
-  unsigned char header[];
+  int checksum;
 } messageHeader;
 
 int calcCheckSum(String message) {
-	int checkSum;
-	
-	for (int i = 0; i < message.length(); i++){
-		checkSum = checkSum + (((int) message.charAt(i)) * i);
-	}
-	
-	return checkSum;
+  int checkSum;
+
+  for (int i = 0; i < message.length(); i++) {
+    checkSum = checkSum + (((int) message.charAt(i)) * i);
+  }
+
+  return checkSum;
 }
 
-void getHeader(String message){
-	
+messageHeader getHeader(byte* message, messageHeader head) {
+  head.checksum = calcCheckSum(String((char*) message));
+  
+  return head;
 }
 
 void setup() {
@@ -40,6 +41,7 @@ void setup() {
 void loop() {
   byte message[VW_MAX_MESSAGE_LEN];
   // N.B. La constante VW_MAX_MESSAGE_LEN est fournie par la lib VirtualWire
+  messageHeader head;
 
   // Lit un message de maximum 26 caractères depuis le port série
   int len = Serial.readBytesUntil('\n', (char*) message, VW_MAX_MESSAGE_LEN - 1);
@@ -47,7 +49,11 @@ void loop() {
     return; // Pas de message
   }
   message[len] = '\0'; // Ferme la chaine de caractères
-
+  
+  head = getHeader(message, head);
+  
+  byte* finalMessage = String(head.id) + String((char*) message) + String(head.checksum);
+  
   vw_send(message, len + 1); // On envoie le message
   vw_wait_tx(); // On attend la fin de l'envoi
 }
