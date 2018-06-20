4c44c69f58db57860214ddaf8c0b7134c612216c
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-06-20 12:54:54 +0200

    pathFinding work

--- Dijkstra/src/Main.java
+++ Dijkstra/src/Main.java
@@ -1,34 +1,31 @@
+import Dijkstra.Dijkstra;
+import Dijkstra.Direction;
+import Dijkstra.Graphe;
+import Dijkstra.OrderSender;
+
 public class Main {
 	public static void main(String[] args) {
-		int N = Graphe.ALPHA_NOTDEF ;
-		int [][] matDuree={
-		                   {N,1,N,N,N,1,N,N,N,N,N,N,N,N,N,N},
-		                   {1,N,2,N,N,N,1,N,N,N,N,N,N,N,N,N},
-		                   {N,2,N,1,N,N,N,1,N,N,N,N,N,N,N,N},
-		                   {N,N,1,N,1,N,N,N,N,2,N,N,N,N,N,N},
-		                   {N,N,N,1,N,N,N,N,N,N,2,N,N,N,N,N},
-		                   {1,N,N,N,N,N,1,N,1,N,N,N,N,N,N,N},
-		                   {N,1,N,N,N,1,N,2,N,N,N,N,N,N,N,N},
-		                   {N,N,1,N,N,N,2,N,N,N,N,N,N,N,N,N},
-		                   {N,N,N,N,N,1,N,N,N,4,N,N,N,2,N,N},
-		                   {N,N,N,2,N,N,N,N,4,N,1,N,1,N,N,N},
-		                   {N,N,N,N,2,N,N,N,N,1,N,N,N,N,N,2},
-		                   {N,N,N,N,N,N,N,N,N,N,N,N,3,N,1,N},
-		                   {N,N,N,N,N,N,N,N,N,1,N,3,N,N,N,N},
-		                   {N,N,N,N,N,N,N,N,2,N,N,N,N,N,1,N},
-		                   {N,N,N,N,N,N,N,N,N,N,N,1,N,1,N,4},
-		                   {N,N,N,N,N,N,N,N,N,N,2,N,N,N,4,N}
-		                  };
-		//création du graphe			
+		int N = Graphe.ALPHA_NOTDEF;
+		int[][] matDuree = { { N, 1, N, N, N, 1, N, N, N, N, N, N, N, N, N, N },
+				{ 1, N, 2, N, N, N, 1, N, N, N, N, N, N, N, N, N }, { N, 2, N, 1, N, N, N, 1, N, N, N, N, N, N, N, N },
+				{ N, N, 1, N, 1, N, N, N, N, 2, N, N, N, N, N, N }, { N, N, N, 1, N, N, N, N, N, N, 2, N, N, N, N, N },
+				{ 1, N, N, N, N, N, 1, N, 1, N, N, N, N, N, N, N }, { N, 1, N, N, N, 1, N, 2, N, N, N, N, N, N, N, N },
+				{ N, N, 1, N, N, N, 2, N, N, N, N, N, N, N, N, N }, { N, N, N, N, N, 1, N, N, N, 4, N, N, N, 2, N, N },
+				{ N, N, N, 2, N, N, N, N, 4, N, 1, N, 1, N, N, N }, { N, N, N, N, 2, N, N, N, N, 1, N, N, N, N, N, 2 },
+				{ N, N, N, N, N, N, N, N, N, N, N, N, 3, N, 1, N }, { N, N, N, N, N, N, N, N, N, 1, N, 3, N, N, N, N },
+				{ N, N, N, N, N, N, N, N, 2, N, N, N, N, N, 1, N }, { N, N, N, N, N, N, N, N, N, N, N, 1, N, 1, N, 4 },
+				{ N, N, N, N, N, N, N, N, N, N, 2, N, N, N, 4, N } };
+		// création du graphe
 		Graphe g0 = new Graphe(matDuree);
-		//LA SUITE		
-		
-		
+		// LA SUITE
+
 		// creation d'une instance de l'algorithme avec le graphe g0
-		Dijkstra beaulieuAutresStations= new Dijkstra(15,g0);
-		
+		Dijkstra dijkstra = new Dijkstra(15, g0);
+
+		dijkstra.afficheChemin(7);
+
 		// Pour afficher le chemin le plus rapide pour aller de beaulieu a Clemenceau
-		beaulieuAutresStations.afficheChemin(7);
+		OrderSender sender = new OrderSender(dijkstra.getChemin(7), Direction.UP);
 	}
 
 }
