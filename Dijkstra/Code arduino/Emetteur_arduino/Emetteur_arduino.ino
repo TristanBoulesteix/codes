a388ae8a01976aac0d42dc3ae2e9080aa758239f
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-06-22 15:21:09 +0200

    uodate

--- Dijkstra/Code arduino/Emetteur_arduino/Emetteur_arduino.ino
+++ Dijkstra/Code arduino/Emetteur_arduino/Emetteur_arduino.ino
@@ -7,19 +7,20 @@ char inputBuffer[10];
 void setup() {
   Serial.begin(9600);
   vw_set_tx_pin(RF_TX_PIN);
-   vw_setup(2000);
+  vw_setup(2000);
 }
 
 void loop() {
-String message; 
-  
+  String message;
+
   while (true) {
     if (Serial.available() > 0) {
       Serial.readBytes(inputBuffer, 10);
-      
+
       delay(5000);
     }
   }
 
   vw_send((byte *) &message, sizeof(message));
+  vw_wait_tx();
 }
