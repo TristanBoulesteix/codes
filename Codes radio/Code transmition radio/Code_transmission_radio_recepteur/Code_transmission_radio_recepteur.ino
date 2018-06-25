d517a2a083075d7ffe7d8774b6eb67e33920930e
Ssaubeck <guillaume.best@viacesi.fr>
2018-06-25 14:59:51 +0200

    Protocole réception terminé

--- Codes radio/Code transmition radio/Code_transmission_radio_recepteur/Code_transmission_radio_recepteur.ino
+++ Codes radio/Code transmition radio/Code_transmission_radio_recepteur/Code_transmission_radio_recepteur.ino
@@ -4,9 +4,6 @@
 int RF_RX_PIN = 2;
 int check;
 
-// String
-String instruction = " ";
-
 
 //Structure
 typedef struct header {
@@ -17,12 +14,14 @@ typedef struct header {
 
 //Variable
 messageHeader comparatif;
+String msg;
 
 int calcCheckSum(String message) {
   int checkSum;
-  for (int i = 0; i < message.length(); i++) {
-    checkSum = checkSum + (((int) message.charAt(i)) * i);
+  for (int s = 0; s < message.length(); s++) {
+    checkSum = checkSum + (((int) message.charAt(s)) * (s+1));
   }
+  Serial.println(checkSum);
   return checkSum;
 }
 
@@ -38,44 +37,50 @@ void setup() {
 }
 
 void loop() {
-  String header= "";
+  byte header[VW_MAX_MESSAGE_LEN];
   byte taille_message = VW_MAX_MESSAGE_LEN;
   // N.B. La constante VW_MAX_MESSAGE_LEN est fournie par la lib VirtualWire
   // On attend de recevoir un message
   vw_wait_rx();
-  if (vw_get_message((byte*)&header, &taille_message)) {
+  if (vw_get_message(header, &taille_message)) {
     Serial.println("Message recu");
-    for( int i=0; i<header.length();i++){
-      int a=0;
-      if (header.charAt(i)== '|'){
-        a++;
-        i++;
-      }
-      if (a==0){
-        comparatif.id+=header.charAt(i);
-      }else if (a==1){
-        comparatif.instruction+= header.charAt(i);
-      }else if (a==2){
-        comparatif.checksum+=header.charAt(i);
-      }
-    }
-    check = calcCheckSum(comparatif.instruction);
-    if (comparatif.id == String(6500)){
-      if (comparatif.checksum==String(check))
-      {
-        Serial.println("Message Correct");
-        Serial.println(comparatif.instruction);
-      }else{
-        Serial.println("Error different checksum");
-        Serial.println(comparatif.checksum);
-        
-      }
-    }else{
-      Serial.println("Error different ID");
-      Serial.println(comparatif.id);
-    }
-  }else {
-    Serial.println("Message corrompu"); // Affiche le message
+    msg=String((char*)header);
+    Serial.println(msg);
+    int a=0;
+        for( int i=0; i<msg.length();i++){
+          if (msg.charAt(i)== '|'){
+            a++;
+            i++;
+          }
+          if (a==0){
+            comparatif.id+=msg.charAt(i);
+            Serial.println(comparatif.id);
+          }else if (a==1){
+            comparatif.instruction+= msg.charAt(i);
+          }else if (a==2){
+            comparatif.checksum+=msg.charAt(i);
+          }
+        }
+        check = calcCheckSum(comparatif.instruction);
+        if (comparatif.id == String(6500)){
+          if (comparatif.checksum==String(check))
+          {
+            Serial.println("Message Correct");
+            Serial.println(comparatif.instruction);
+          }else{
+            Serial.println("Error different checksum");
+            Serial.println(comparatif.checksum);
+    
+          }
+        }else{
+          Serial.println("Error different ID");
+          Serial.println(comparatif.id);
+        }
+      }else {
+        Serial.println("Message corrompu"); // Affiche le message
   }
+  comparatif.id="";
+  comparatif.checksum="";
+  comparatif.instruction="";
   
 }
