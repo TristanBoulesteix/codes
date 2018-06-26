de53dbe8b5c0fe0cd77561d55ba2c2573dc216bc
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-06-26 11:27:38 +0200

    Update with header

--- Lorann-Arduino-Controller/src/arduino/lorann/arduino/ArduinoConnector.java
+++ Lorann-Arduino-Controller/src/arduino/lorann/arduino/ArduinoConnector.java
@@ -10,12 +10,32 @@ import gnu.io.SerialPort;
 import gnu.io.UnsupportedCommOperationException;
 
 public class ArduinoConnector {
+	private static final int ID = 6500;
+
 	private static Enumeration portList;
 	private static CommPortIdentifier portId;
 	private static SerialPort serialPort;
 	private static OutputStream outputStream;
 
+	private static StringBuilder stringToBuild;
+
+	private static int calcCheckSum(String message) {
+		int checkSum = 0;
+		for (int i = 0; i < message.length(); i++) {
+			checkSum = checkSum + (((int) message.charAt(i)) * (i + 1));
+		}
+		return checkSum;
+	}
+
 	public static void sendToArduino(String messageString) {
+		stringToBuild = new StringBuilder();
+
+		stringToBuild.append(ID);
+		stringToBuild.append("|");
+		stringToBuild.append(messageString);
+		stringToBuild.append("|");
+		stringToBuild.append(calcCheckSum(messageString));
+
 		portList = CommPortIdentifier.getPortIdentifiers();
 
 		while (portList.hasMoreElements()) {
@@ -43,8 +63,8 @@ public class ArduinoConnector {
 						e.printStackTrace();
 					}
 					try {
-						outputStream.write(messageString.getBytes());
-						System.out.println("Message envoyé :" + messageString);
+						outputStream.write(stringToBuild.toString().getBytes());
+						System.out.println("Instruction envoyée :" + messageString);
 
 						outputStream.close();
 						serialPort.close();
